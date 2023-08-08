9e225e9fdfc4a05f0a9f30e4d89aae0fcdc561a1
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2023-08-08 11:22:39 -0300

    Controller

--- src/main/java/com/david/messaging/MessagingApplication.java
+++ src/main/java/com/david/messaging/MessagingApplication.java
@@ -2,6 +2,10 @@ package com.david.messaging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MessagingApplication {
@@ -10,4 +14,21 @@ public class MessagingApplication {
 		SpringApplication.run(MessagingApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequestMapping("/kafka")
+class HelloController {
+
+	private HelloProducer helloProducer;
+
+	public HelloController(HelloProducer helloProducer) {
+		this.helloProducer = helloProducer;
+	}
+
+	@GetMapping("/hello/{name}")
+	public String hello(@PathVariable("name") String name) {
+		helloProducer.sendMessage("Hello, " + name);
+		return "Ok!";
+	}
 }
